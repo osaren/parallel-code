8fae388c5736221fd0a536604bd1233d677d8bbc
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-10 21:04:43 +0000

    Create PARALLEL - process_fish_rules.c

---
+++ PARALLEL - process_fish_rules.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void processFishRules(int *current, int *neighbours) {
+  //printf("Current that was passed is: %d\n", *current);
+  // process fish rules
+
+  // Declare variables
+  // These variables will count how many neighbors are fish/sharks
+  int sharkNeighbours = 0;
+  int fishNeighbours = 0;
+
+  // Loop over the neighbors array
+  // so we can count how many neighbors are sharks/fish
+  for (int j = 0; j < 8; j++){
+    // if neighbour is shark
+    if (neighbours[j] <= -1) { // neighbour is shark
+      sharkNeighbours++;
+    } else if (neighbours[j] >= 1) { // neighbour is fish
+      fishNeighbours++;
+    }
+  }
+
+  // is fish too old
+  if (*current >= 10) {
+    *current = 0; // kill fish due to age
+  } else if (sharkNeighbours >= 5){
+    //printf("fish is overcrowded");
+    *current = 0; // kills the fish, due to predators
+  } else if (fishNeighbours >= 8) {
+    *current = 0; // kills the fish, due to overcrowding
+  } else {
+    *current = *current + 1;
+  }
+}
