688536cae3e6cfe243ad5dacbe9396351e114270
dylanforman <73077849+dylanforman@users.noreply.github.com>
2020-12-11 12:45:03 +0000

    Update PARALLEL - process_ocean_row.c

--- PARALLEL - process_ocean_row.c
+++ PARALLEL - process_ocean_row.c
@@ -1,6 +1,77 @@
 #include <stdlib.h>
 #include "functions.h"
 
+void* getNeighbors(long my_row, currentColumn)
+{   
+     int *neighbours;
+     int amountOfNeighbours;
+
+    // inside getNeighbours
+    // Based on my_row and currentColumn
+    // Determine if boundary or not
+    if(my_row == 1) // its a boundary cell - Row 1 (top row)
+    {
+      if(currentColumn == 1 || currentColumn = 20000)// [1][1] Top Left Corner cell [1][20000] Top Right Corner Cell - only 3 neighbors
+      {
+        amountOfNeighbours = 3; 
+        *neighbors = malloc(sizeof(int[amountOfNeighbours]));
+        if(currentColumn == 1) // gets the neighbors for the top left cell
+        {
+          *neighbours = {extern ocean[my_row][currentColumn+1], extern ocean[my_row+1][currentColumn], extern ocean[my_row+1][currentColumn+1]};
+        }
+        else if(currentColumn == 20000) // gets the neighbors for the top right cell
+        {
+          *neighbours = {extern ocean[my_row][currentColumn-1], extern ocean[my_row-1][currentColumn], extern ocean[my_row-1][currentColumn-1]};
+        }
+       }
+      else 
+      {
+       amountOfNeighbours = 5;  // rest of the cells on row 1 have 5 neighbors
+       *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+       *neighbours = {extern ocean[my_row][currentColumn-1], extern ocean [my_row][currentColumn+1], extern ocean [my_row+1][currentColumn-1], 
+                      extern ocean [my_row+1][currentColumn], extern ocean[my_row+1][currentColumn+1]}; // gets the 5 neighbours for the rest of the cells on row 1
+                      
+      }
+    }
+    else if(my_row == 20000) // its a boundary cell - Bottom Row
+    {
+      if(currentColumn == 1 || currentColumn = 20000) // [20000][1] Bottom Left Corner cell. [20000][20000] Bottom Right Corner cell - only 3 neighbours
+      {
+        amountOfNeighbours = 3;
+        *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+      }
+      else 
+      {
+        amountOfNeighbours = 5; // rest of the cells on row 20000 have 5 neighbours
+        *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+      }
+    }
+    else if(currentColumn == 1) // its  a boundary cell - Left most column
+    {
+     *amountOfNeighbours = 5; 
+      *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+    }
+    else if(currentColumn == 20000) // its a boundary cell - Right most column
+    {
+      amountOfNeighbours =5;
+      *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+    }
+    else // its not a boundary cell
+    {
+      amountOfNeighbours = 8;
+      *neighbours = malloc(sizeof(int[amountOfNeighbours]));
+    }
+    // e.g has 3 neighbours, malloc on the pointer
+    // so that neighbours becomes an array which can store 3 ints
+    // and then get those neighbours and put into the array
+  // TODO remove neighbours array that is hardcoded to 8 elements,
+    // in favour of pointer
+    // int *neighbours;
+    // int *amountOfNeighbours;
+
+  
+}
+
 void* processOceanRow(void *row) {
 
   // Cast provided row to a long
