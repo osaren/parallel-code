d86b75a22bff1b0b140abd8ad81b3f93b111f313
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-12 23:45:08 +0000

    Update PARALLEL - functions.h

--- PARALLEL - functions.h
+++ PARALLEL - functions.h
@@ -1,8 +1,8 @@
 // Global variables
-#define THREAD_COUNT 1
-#define OCEAN_ROW_SIZE 400
-#define OCEAN_COLUMN_SIZE 400
-#define TARGET_GENERATIONS 10000
+#define THREAD_COUNT 10
+#define OCEAN_ROW_SIZE 1000
+#define OCEAN_COLUMN_SIZE 1000
+#define TARGET_GENERATIONS 20000
 #define EMPTY_CELL 0
 
 extern int ocean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE]; // Declares an array, without any values
