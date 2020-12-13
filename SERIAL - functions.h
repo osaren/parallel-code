203bd0f9e88f4597d234ebe0e81f8bd3f5632ab5
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-13 13:12:45 +0000

    Update SERIAL - functions.h

--- SERIAL - functions.h
+++ SERIAL - functions.h
@@ -1,4 +1,10 @@
 // Global variables
+#define OCEAN_ROW_SIZE 1000
+#define OCEAN_COLUMN_SIZE 1000
+#define TARGET_GENERATIONS 20000
+#define EMPTY_CELL 0
+
+
 int ocean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE]; // Declares an array, without any values
 int futureOcean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE]; // Declares an array, without any values
 // int ocean[2][2] = { // Example, of declaring a 2D array, and setting values
@@ -6,9 +12,11 @@ int futureOcean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE]; // Declares an array, withou
   // {3, 4}
 //   };
 
-int shark = -1;
-int fish = 1;
-int empty = 0;
-
 // Function declarations
 void initialPopulation();
+void Print_matrix(char *title);
+void processSharkRules(int *current, int *neighbours, int amountOfNeighbours);
+void processFishRules(int *current, int *neighbours, int amountOfNeighbours);
+void processBreedingRules(int *current, int *neighbours, int amountOfNeighbours);
+int* getNeighbours(long currentRow, int currentColumn, int *neighbours, int* amountOfNeighbours);
+
