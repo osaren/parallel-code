2c07ffbc13a6c25f0ec9f95da0bca1eaa0e1639e
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-13 13:11:50 +0000

    Update SERIAL - fishes_and_sharks.c

--- SERIAL - fishes_and_sharks.c
+++ SERIAL - fishes_and_sharks.c
@@ -1,9 +1,11 @@
-#define OCEAN_ROW_SIZE 1000
-#define OCEAN_COLUMN_SIZE 1000
-#define TARGET_GENERATIONS 20000
-
 #include <stdio.h>
 #include <stdlib.h>
+
+// Header files for time output
+#include <time.h>
+#include <string.h>
+// END
+ 
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
@@ -23,7 +25,11 @@ int main(int argc, char *argv[]) {
   printf("numberOfColumns: %d.\n", OCEAN_COLUMN_SIZE);
 
   initialPopulation();
-
+  
+  time_t mytime = time(NULL);
+  char * time_str = ctime(&mytime);
+  time_str[strlen(time_str) - 1] = '\0';
+  printf("Current time: %s\n", time_str);
 
   // Loop through each generation
   for (int currentGeneration = 0; currentGeneration < TARGET_GENERATIONS; currentGeneration++) {
@@ -31,147 +37,145 @@ int main(int argc, char *argv[]) {
 
     // Loop through each cell, in order to process rules
     // Dont process boundary cells (for now)
-    for (int currentRow = 1; currentRow < OCEAN_ROW_SIZE - 1; currentRow++) {
-      for (int currentColumn = 1; currentColumn < OCEAN_COLUMN_SIZE - 1; currentColumn++) {
+    for (int currentRow = 0; currentRow < OCEAN_ROW_SIZE; currentRow++) {
+      for (int currentColumn = 0; currentColumn < OCEAN_COLUMN_SIZE; currentColumn++) {
 
-        // TODO create a new thread
-        // each thread, will process a single row from the ocean
-        // only 5 threads running at a time
+    // get current
+    int current = ocean[currentRow][currentColumn];
 
-        // get current
-        int current = ocean[currentRow][currentColumn];
+    // get all the current cells neighbours
+    int *neighbours; // Populated inside getNeighbours()
+    int amountOfNeighbours; // Populated inside getNeighbours()
 
-        // get all the current cells neighbours
-        // Doesn't take into consideration boundary cells (for now)
-        int neighbours[8];
-        neighbours[0] = ocean[currentRow - 1][currentColumn - 1];
+    int* getNeighbours(long currentRow, int currentColumn, int *neighbours, int *amountOfNeighbours) {
+    // Based on my_row and currentColumn
+    // Determine if boundary or not
+    if (currentRow == 0) { // its a boundary cell - Row 0 (top row)
+
+    // [0][0] Top Left Corner cell
+    // [0][OCEAN_COLUMN_SIZE - 1] Top Right Corner Cell
+    // - only 3 neighbours
+    if (currentColumn == 0 || currentColumn == (OCEAN_COLUMN_SIZE - 1)) {
+
+      *amountOfNeighbours = 3;
+      neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+      if(currentColumn == 0) { // gets the neighbors for the top left cell
+
+        neighbours[0] = ocean[currentRow][currentColumn + 1];
+        neighbours[1] = ocean[currentRow + 1][currentColumn];
+        neighbours[2] = ocean[currentRow + 1][currentColumn + 1];
+
+      } else if(currentColumn == (OCEAN_COLUMN_SIZE - 1)) { // gets the neighbors for the top right cell
+
+        neighbours[0] = ocean[currentRow][currentColumn - 1];
+        neighbours[1] = ocean[currentRow + 1][currentColumn];
+        neighbours[2] = ocean[currentRow + 1][currentColumn - 1];
+
+      }
+    } else {
+
+      *amountOfNeighbours = 5;  // rest of the cells on row 0 have 5 neighbors
+      neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+      // gets the 5 neighbours for the rest of the cells on row 0
+      neighbours[0] = ocean[currentRow][currentColumn - 1];
+      neighbours[1] = ocean[currentRow][currentColumn + 1];
+      neighbours[2] = ocean[currentRow + 1][currentColumn - 1];
+      neighbours[3] = ocean[currentRow + 1][currentColumn];
+      neighbours[4] = ocean[currentRow + 1][currentColumn + 1];
+    }
+
+  } else if (currentRow == (OCEAN_ROW_SIZE - 1)) { // its a boundary cell - Bottom Row
+
+    // [OCEAN_ROW_SIZE - 1][0] Bottom Left Corner cell.
+    // [OCEAN_ROW_SIZE - 1][OCEAN_COLUMN_SIZE - 1] Bottom Right Corner cell
+    // - only 3 neighbours
+    if (currentColumn == 0 || currentColumn == (OCEAN_COLUMN_SIZE - 1)) {
+
+      *amountOfNeighbours = 3;
+      neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+      // gets the neighbours for the bottom left corner cell
+      if (currentColumn == 0) {
+
+        neighbours[0] = ocean[currentRow - 1][currentColumn];
+        neighbours[1] = ocean[currentRow - 1][currentColumn + 1];
+        neighbours[2] = ocean[currentRow][currentColumn + 1];
+
+      } else if(currentColumn == (OCEAN_COLUMN_SIZE - 1)) {
+        // gets the neighbours for the bottom right corner cell
+        neighbours[0] = ocean[currentRow][currentColumn - 1];
         neighbours[1] = ocean[currentRow - 1][currentColumn];
-        neighbours[2] = ocean[currentRow - 1][currentColumn + 1];
-        neighbours[3] = ocean[currentRow][currentColumn - 1];
-        neighbours[4] = ocean[currentRow][currentColumn + 1];
-        neighbours[5] = ocean[currentRow + 1][currentColumn - 1];
-        neighbours[6] = ocean[currentRow + 1][currentColumn];
-        neighbours[7] = ocean[currentRow + 1][currentColumn + 1];
-        // neighbours = {1, 5, 0, 6, -8, 2, -5, 1}
+        neighbours[2] = ocean[currentRow - 1][currentColumn - 1];
+      }
+
+    } else {
+      // rest of the cells on row OCEAN_ROW_SIZE - 1, have 5 neighbours
+      *amountOfNeighbours = 5;
+      neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+      neighbours[0] = ocean[currentRow][currentColumn - 1];
+      neighbours[1] = ocean[currentRow][currentColumn + 1];
+      neighbours[2] = ocean[currentRow - 1][currentColumn - 1];
+      neighbours[3] = ocean[currentRow - 1][currentColumn];
+      neighbours[4] = ocean[currentRow - 1][currentColumn + 1];
+    }
+
+  } else if(currentColumn == 0 && currentRow != 0 && currentRow != (OCEAN_ROW_SIZE - 1)) {
+
+    // its  a boundary cell -
+    // Left most column AND not the top left or bottom left corner cell
+    *amountOfNeighbours = 5;
+    neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+    neighbours[0] = ocean[currentRow - 1][currentColumn];
+    neighbours[1] = ocean[currentRow - 1][currentColumn + 1];
+    neighbours[2] = ocean[currentRow][currentColumn + 1];
+    neighbours[3] = ocean[currentRow + 1][currentColumn];
+    neighbours[4] = ocean[currentRow + 1][currentColumn + 1];
+
+  } else if (currentColumn == (OCEAN_COLUMN_SIZE - 1) && currentRow != 0 && currentRow != (OCEAN_ROW_SIZE - 1)) {
+
+    // its a boundary cell -
+    // Right most column AND not the top right or bottom right corner cell
+    *amountOfNeighbours = 5;
+    neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+    neighbours[0] = ocean[currentRow - 1][currentColumn - 1];
+    neighbours[1] = ocean[currentRow - 1][currentColumn];
+    neighbours[2] = ocean[currentRow][currentColumn - 1];
+    neighbours[3] = ocean[currentRow + 1][currentColumn - 1];
+    neighbours[4] = ocean[currentRow + 1][currentColumn];
+
+  } else { // its not a boundary cell
+
+    *amountOfNeighbours = 8;
+    neighbours = malloc(*amountOfNeighbours * sizeof(int));
+
+    neighbours[0] = ocean[currentRow - 1][currentColumn - 1];
+    neighbours[1] = ocean[currentRow - 1][currentColumn];
+    neighbours[2] = ocean[currentRow - 1][currentColumn + 1];
+    neighbours[3] = ocean[currentRow][currentColumn - 1];
+    neighbours[4] = ocean[currentRow][currentColumn + 1];
+    neighbours[5] = ocean[currentRow + 1][currentColumn - 1];
+    neighbours[6] = ocean[currentRow + 1][currentColumn];
+    neighbours[7] = ocean[currentRow + 1][currentColumn + 1];
+  }
+
+  return neighbours;
+}
 
         // process rules
         if (current <= -1) {
-
-          // process shark rules
-
-          // Declare variables
-          // These variables will count how many neighbors are fish/sharks
-          int sharkNeighbours = 0;
-          int fishNeighbours = 0;
-
-	        // Loop over the neighbors array
-          // so we can count how many neighbors are sharks/fish
-          for (int j = 0; j < 8; j++){
-            // if neighbour is shark
-            if (neighbours[j] <= -1) { // neighbour is shark
-              sharkNeighbours++;
-            } else if (neighbours[j] >= 1) { // neighbour is fish
-              fishNeighbours++;
-            }
-          }
-          // sharkNeighbours = 6
-          // fishNeighbours = 0
-
-          // should randomly die (0.031 chance of dying)
-          float min = 0.0;
-          float max = 100.0;
-          float scale = (float) rand() / (float) RAND_MAX;
-          float randomChanceOfDeath = min + scale * (max - min);
-          if (randomChanceOfDeath < 0.031) {
-            current = 0; // kills the shark randomly
-          } else {
-            if (current <= -20) { // if shark is older than -20
-              //printf("Shark is too old");
-              current = 0; // kills the shark, due to age
-            } else if (sharkNeighbours >= 6 && fishNeighbours == 0){
-              //printf("Shark is overcrowded");
-              current = 0; // kills the shark, due to starvation
-            } else {
-              current++;
-            }
-          }
-
-          // we've processed the shark rules for current
-          // and we know what value current will become in the next generation
-
-        } else if(current >= 1){ // this is a fish
-          // process fish rules
-
-          // Declare variables
-          // These variables will count how many neighbors are fish/sharks
-          int sharkNeighbours = 0;
-          int fishNeighbours = 0;
-
-	        // Loop over the neighbors array
-          // so we can count how many neighbors are sharks/fish
-          for (int j = 0; j < 8; j++){
-            // if neighbour is shark
-            if (neighbours[j] <= -1) { // neighbour is shark
-              sharkNeighbours++;
-            } else if (neighbours[j] >= 1) { // neighbour is fish
-              fishNeighbours++;
-            }
-          }
-
-          // is fish too old
-          if (current >= 10) {
-            current = 0; // kill fish due to age
-          } else if (sharkNeighbours >= 5){
-            //printf("fish is overcrowded");
-            current = 0; // kills the fish, due to predators
-          } else if (fishNeighbours >= 8) {
-            current = 0; // kills the fish, due to overcrowding
-          } else {
-            current++;
-          }
-
-        } else if(current == 0) { // this is empty
-          // process breeding rules
-
-          // Declare variables
-          // These variables will count how many neighbors are fish/sharks
-          int sharkNeighbours = 0;
-          int sharksOfBreedingAge = 0;
-          int fishNeighbours = 0;
-          int fishesOfBreedingAge = 0;
-
-	        // Loop over the neighbors array
-          // so we can count how many neighbors are sharks/fish
-          for (int j = 0; j < 8; j++){
-            // if neighbour is shark
-            if (neighbours[j] <= -1) { // neighbour is shark
-              sharkNeighbours++;
-
-              // check if shark neighbour is of a breeding age
-              if (neighbours[j] <= -3) {
-                // if so, increment counter
-                sharksOfBreedingAge++;
-              }
-
-            } else if (neighbours[j] >= 1) { // neighbour is fish
-              fishNeighbours++;
-
-              // check if fish neighbour is of a breeding age
-              if (neighbours[j] >= 2) {
-                fishesOfBreedingAge++;
-              }
-            }
-          }
-
-          if (sharkNeighbours >= 4 && fishNeighbours < 4 && sharksOfBreedingAge >= 3) {
-            // breed a shark
-            current = -1;
-          } else if (fishNeighbours >= 4 && sharkNeighbours < 4 && fishesOfBreedingAge >= 3) {
-            // breed a fish
-            current = 1;
-          }
-        }
-
+	  processSharkRules(&current, neighbours, amountOfNeighbours);
+	} else if(current >= 1){
+	  processFishRules(&current, neighbours, amountOfNeighbours);
+	} else if(current == 0){
+	  processBreedingRules(&current, neighbours, amountOfNeighbours);
+	}
+     
+          
         // put new value into futureOcean, at the same position
         futureOcean[currentRow][currentColumn] = current;
       }
@@ -189,5 +193,9 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  mytime = time(NULL);
+  time_str = ctime(&mytime);
+  time_str[strlen(time_str) - 1] = '\0';
+  printf("Current time: %s\n", time_str);
+  
   return 0;
-}
