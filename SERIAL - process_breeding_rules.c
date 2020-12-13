091c38be322161df52f32758d169a707ff85b2f9
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-13 13:15:24 +0000

    Create SERIAL - process_breeding_rules.c

---
+++ SERIAL - process_breeding_rules.c
@@ -0,0 +1,41 @@
+void processBreedingRules(int *current, int *neighbours, int amountOfNeighbours) {
+  // process breeding rules
+
+  // Declare variables
+  // These variables will count how many neighbors are fish/sharks
+  int sharkNeighbours = 0;
+  int sharksOfBreedingAge = 0;
+  int fishNeighbours = 0;
+  int fishesOfBreedingAge = 0;
+
+  // Loop over the neighbors array
+  // so we can count how many neighbors are sharks/fish
+  for (int j = 0; j < amountOfNeighbours; j++){
+    // if neighbour is shark
+    if (neighbours[j] <= -1) { // neighbour is shark
+      sharkNeighbours++;
+
+      // check if shark neighbour is of a breeding age
+      if (neighbours[j] <= -3) {
+        // if so, increment counter
+        sharksOfBreedingAge++;
+      }
+
+    } else if (neighbours[j] >= 1) { // neighbour is fish
+      fishNeighbours++;
+
+      // check if fish neighbour is of a breeding age
+      if (neighbours[j] >= 2) {
+        fishesOfBreedingAge++;
+      }
+    }
+  }
+
+  if (sharkNeighbours >= 4 && fishNeighbours < 4 && sharksOfBreedingAge >= 3) {
+    // breed a shark
+    *current = -1;
+  } else if (fishNeighbours >= 4 && sharkNeighbours < 4 && fishesOfBreedingAge >= 3) {
+    // breed a fish
+    *current = 1;
+  }
+}
