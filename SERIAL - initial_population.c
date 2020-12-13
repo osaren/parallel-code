153389bb9837c40b8391d7732b4bcac789831dec
B00097410-RoisinEccles <63256617+B00097410-RoisinEccles@users.noreply.github.com>
2020-12-13 13:14:01 +0000

    Update SERIAL - initial_population.c

--- SERIAL - initial_population.c
+++ SERIAL - initial_population.c
@@ -1,15 +1,11 @@
-#define OCEAN_ROW_SIZE 1000
-#define OCEAN_COLUMN_SIZE 1000
-#define TARGET_GENERATIONS 20000
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
 
 extern int ocean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE];
-extern int futureOcean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE];
+extern int futureOcean[OCEAN_ROW_SIZE][OCEAN_COLUMN_SIZE]; 
 extern int shark;
 extern int fish;
-extern int empty;
 
 void initialPopulation() {
   // Outter for loop, responible for walking down the rows in the 2D array
@@ -20,7 +16,7 @@ void initialPopulation() {
     // Inner for loop, responible for walking across columns in the 2D array
     // from left to right
     for (int j = 0; j < OCEAN_COLUMN_SIZE; j++) {
-      ocean[i][j] = empty;
+      ocean[i][j] = EMPTY_CELL;
     }
   }
 
@@ -46,5 +42,5 @@ void initialPopulation() {
     }
   }
   printf("Finished populating initial state of ocean.\n");
-  // ocean has been populated with empty cells, fishes and sharks.
+//ocean has been populated with empty cells, fishes and sharks.
 }
